225d9e637fcdc9de233a28a1a6a3ff00c7a87c5a
Christina Ligare <cligare@hotmail.com>
2025-12-05 10:50:39 -0800

    Task Manager API: learned MongoDB and Mongoose basics

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,35 @@
-console.log('Task Manager App')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks.js");
+const connectDB = require("./db/connect.js");
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+//app.get("/api/v1/tasks")        - gel all the tasks
+//app.post("/api/v1/tasks")       - create a new task
+//app.get("/api/v1/tasks/:id")    - get single task
+//app.patch("/api/v1/tasks/:id")  - update task
+//app.delete("/api/v1/tasks/:id") - delete task
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port: ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
