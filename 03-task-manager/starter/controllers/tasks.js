7b9f124a0ecf35af089d6067cb713f23fb64df4b
Christina Ligare <cligare@hotmail.com>
2025-12-10 10:42:04 -0800

    Added input validation and error handling; refactored try/catch using asyncWrapper

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,25 +1,65 @@
 const Task = require("../models/Task");
+const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error.js");
 
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+  // res.status(200).json({tasks, amount:tasks.length })
+  // res
+  //   .status(200)
+  //   .json({ success: true, data: { tasks, nbHits: tasks.length } });
+  res.status(500).json({ msg: error });
+});
 
-const createTask = async (req, res) => {
+const createTask = asyncWrapper(async (req, res) => {
   const task = await Task.create(req.body);
   res.status(201).json({ task });
-};
+});
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
-};
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params;
+  // _id is always a Mongo ObjectId by default
+  const task = await Task.findOne({ _id: taskID });
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
+  }
+  res.status(200).json({ task });
+});
 
-const updateTask = (req, res) => {
-  res.send("update task");
-};
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    // Message when id format is correct but no matching task exists
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
+  }
+  res.status(200).json({ task });
+});
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
-};
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  // By adding the new:true, runValidators:true options, I make sure that the data is updated when running patch in Postman
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
+  }
+  res.status(200).json({ task });
+});
+
+//Note: In the getTask function before refactoring with the asyncWrapper, I had this comment that I want to keep for now:
+// } catch (error) {
+// Error happens when the id format is invalid (not the correct length/shape - meaning not a hexadecimal 0-9, a -f)
+//   res.status(500).json({ msg: error });
+// }
+
+//I also want to keep this comment for now:
+// Message when id format is correct but no matching task exists
+//return res.status(404).json({ msg: `No task with id : ${taskID}` });
 
 module.exports = {
   getAllTasks,
